d047ca9afd4cfcdd8d989e1864000c3dc288c722
RotatingCloud <113541115+RotatingCloud@users.noreply.github.com>
2024-09-14 12:10:46 -0700

    chore: Add initial project files and dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,ts,tsx,js,jsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
